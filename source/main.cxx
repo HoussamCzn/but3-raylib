aa8e205cd0d8dbbd35674367f09a0d2e39ea9798
Houssam CUMZAIN <doriancoriace@gmail.com>
2023-12-19 10:55:23 +0100

    feat: add surface normals rendering

--- source/main.cxx
+++ source/main.cxx
@@ -4,23 +4,30 @@
 
 #include <iostream> // std::cout, std::clog, std::flush
 
-auto hit_sphere(point3 const& center, double radius, ray const& r) -> bool
+auto hit_sphere(point3 const& center, double radius, ray const& r) -> double
 {
-    vec3 oc = r.origin() - center;
-    auto a = dot(r.direction(), r.direction());
-    auto b = 2.0 * dot(oc, r.direction());
-    auto c = dot(oc, oc) - radius * radius;
-    auto discriminant = b * b - 4 * a * c;
+    vec3 const oc = r.origin() - center;
+    auto const a = dot(r.direction(), r.direction());
+    auto const b = 2.0 * dot(oc, r.direction());
+    auto const c = dot(oc, oc) - radius * radius;
+    auto const discriminant = b * b - 4 * a * c;
 
-    return (discriminant >= 0);
+    if (discriminant < 0) { return -1.0; }
+    return (-b - sqrt(discriminant)) / (2.0 * a);
 }
 
 auto ray_color(ray const& r) -> color
 {
-    if (hit_sphere(point3(0, 0, -1), 0.5, r)) return {1, 0, 0};
+    auto const t = hit_sphere(point3(0, 0, -1), 0.5, r);
 
-    vec3 unit_direction = unit_vector(r.direction());
-    auto a = 0.5 * (unit_direction.y() + 1.0);
+    if (t > 0.0)
+    {
+        auto const n = unit_vector(r.at(t) - vec3(0, 0, -1));
+        return 0.5 * color(n.x() + 1, n.y() + 1, n.z() + 1);
+    }
+
+    vec3 const unit_direction = unit_vector(r.direction());
+    auto const a = 0.5 * (unit_direction.y() + 1.0);
 
     return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
 }
