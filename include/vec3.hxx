4d688154349b68bed06ef050b02c47cda8f89426
Houssam CUMZAIN <doriancoriace@gmail.com>
2023-12-19 10:28:11 +0100

    refactor(vec3): add array as a private member

--- include/vec3.hxx
+++ include/vec3.hxx
@@ -8,71 +8,84 @@ class vec3
 {
 public:
 
-    std::array<double, 3> e{};
+    constexpr vec3() : m_e{0, 0, 0} {}
 
-    constexpr vec3() : e{0, 0, 0} {}
+    constexpr vec3(double e0, double e1, double e2) : m_e{e0, e1, e2} {}
 
-    constexpr vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
+    auto x() const -> double { return m_e[0]; }
 
-    auto x() const -> double { return e[0]; }
+    auto y() const -> double { return m_e[1]; }
 
-    auto y() const -> double { return e[1]; }
-
-    auto z() const -> double { return e[2]; }
+    auto z() const -> double { return m_e[2]; }
 
     auto length() const -> double { return std::sqrt(length_squared()); }
 
-    auto length_squared() const -> double { return e[0] * e[0] + e[1] * e[1] + e[2] * e[2]; }
+    auto length_squared() const -> double { return m_e[0] * m_e[0] + m_e[1] * m_e[1] + m_e[2] * m_e[2]; }
 
-    auto operator-() const -> vec3 { return {-e[0], -e[1], -e[2]}; }
+    auto operator-() const -> vec3 { return {-m_e[0], -m_e[1], -m_e[2]}; }
 
-    auto operator[](int i) const -> double { return e[i]; }
+    auto operator[](int i) const -> double { return m_e[i]; }
 
-    auto operator[](int i) -> double& { return e[i]; }
+    auto operator[](int i) -> double& { return m_e[i]; }
 
     auto operator+=(vec3 const& v) -> vec3&
     {
-        e[0] += v.e[0];
-        e[1] += v.e[1];
-        e[2] += v.e[2];
+        m_e[0] += v.m_e[0];
+        m_e[1] += v.m_e[1];
+        m_e[2] += v.m_e[2];
+
         return *this;
     }
 
     auto operator*=(double t) -> vec3&
     {
-        e[0] *= t;
-        e[1] *= t;
-        e[2] *= t;
+        m_e[0] *= t;
+        m_e[1] *= t;
+        m_e[2] *= t;
+
         return *this;
     }
 
     auto operator/=(double t) -> vec3& { return *this *= 1 / t; }
+
+    friend inline auto operator<<(std::ostream& out, vec3 const& v) -> std::ostream&
+    {
+        return out << v.m_e[0] << ' ' << v.m_e[1] << ' ' << v.m_e[2];
+    }
+
+    friend inline auto operator+(vec3 const& u, vec3 const& v) -> vec3
+    {
+        return {u.m_e[0] + v.m_e[0], u.m_e[1] + v.m_e[1], u.m_e[2] + v.m_e[2]};
+    }
+
+    friend inline auto operator-(vec3 const& u, vec3 const& v) -> vec3
+    {
+        return {u.m_e[0] - v.m_e[0], u.m_e[1] - v.m_e[1], u.m_e[2] - v.m_e[2]};
+    }
+
+    friend inline auto operator*(vec3 const& u, vec3 const& v) -> vec3
+    {
+        return {u.m_e[0] * v.m_e[0], u.m_e[1] * v.m_e[1], u.m_e[2] * v.m_e[2]};
+    }
+
+    friend inline auto operator*(double t, vec3 const& v) -> vec3 { return {t * v.m_e[0], t * v.m_e[1], t * v.m_e[2]}; }
+
+    friend inline auto operator*(vec3 const& v, double t) -> vec3 { return t * v; }
+
+    friend inline auto operator/(vec3 v, double t) -> vec3 { return (1 / t) * v; }
+
+private:
+
+    std::array<double, 3> m_e;
 };
 
 // Vector Utility Functions
 
-inline auto operator<<(std::ostream& out, vec3 const& v) -> std::ostream&
-{
-    return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2];
-}
-
-inline auto operator+(vec3 const& u, vec3 const& v) -> vec3 { return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]); }
-
-inline auto operator-(vec3 const& u, vec3 const& v) -> vec3 { return vec3(u.e[0] - v.e[0], u.e[1] - v.e[1], u.e[2] - v.e[2]); }
-
-inline auto operator*(vec3 const& u, vec3 const& v) -> vec3 { return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]); }
-
-inline auto operator*(double t, vec3 const& v) -> vec3 { return vec3(t * v.e[0], t * v.e[1], t * v.e[2]); }
-
-inline auto operator*(vec3 const& v, double t) -> vec3 { return t * v; }
-
-inline auto operator/(vec3 v, double t) -> vec3 { return (1 / t) * v; }
-
-inline auto dot(vec3 const& u, vec3 const& v) -> double { return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2]; }
+inline auto dot(vec3 const& u, vec3 const& v) -> double { return u.x() * v.x() + u.y() * v.y() + u.z() * v.z(); }
 
 inline auto cross(vec3 const& u, vec3 const& v) -> vec3
 {
-    return {u.e[1] * v.e[2] - u.e[2] * v.e[1], u.e[2] * v.e[0] - u.e[0] * v.e[2], u.e[0] * v.e[1] - u.e[1] * v.e[0]};
+    return {u.x() * v.y() - u.y() * v.x(), u.y() * v.z() - u.z() * v.y(), u.z() * v.x() - u.x() * v.z()};
 }
 
 inline auto unit_vector(vec3 v) -> vec3 { return v / v.length(); }
