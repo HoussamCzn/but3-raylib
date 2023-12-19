33e87d9a5e722bde7ac44b32d2d14e342aaa6809
Houssam CUMZAIN <doriancoriace@gmail.com>
2023-12-19 10:06:46 +0100

    feat: use color functions instead of old main

--- source/main.cxx
+++ source/main.cxx
@@ -1,3 +1,6 @@
+#include "color.hxx" // color
+#include "vec3.hxx"  // vec3
+
 #include <iostream> // std::cout, std::clog, std::flush
 
 auto main() -> int
@@ -7,18 +10,16 @@ auto main() -> int
     int image_height = 256;
     // Render
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
     for (int j = 0; j < image_height; ++j)
     {
         std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+
         for (int i = 0; i < image_width; ++i)
         {
-            auto r = static_cast<double>(i) / (image_width - 1);
-            auto g = static_cast<double>(j) / (image_height - 1);
-            auto b = 0;
-            int ir = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+            auto const pixel_color =
+                color{static_cast<double>(i) / (image_width - 1), static_cast<double>(j) / (image_height - 1), 0};
+            write_color(std::cout, pixel_color);
         }
     }
     std::clog << "\rDone.                 \n";
