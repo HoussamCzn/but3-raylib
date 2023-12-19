f7add3825cb26d00564c8cee7bc69154ff9f89d2
Houssam CUMZAIN <doriancoriace@gmail.com>
2023-12-19 10:31:35 +0100

    feat: change aspect ratio and width for main example

--- source/main.cxx
+++ source/main.cxx
@@ -1,12 +1,36 @@
 #include "color.hxx" // color
+#include "ray.hxx"   // point3
 
 #include <iostream> // std::cout, std::clog, std::flush
 
 auto main() -> int
 {
     // Image
-    int image_width = 256;
-    int image_height = 256;
+    auto aspect_ratio = 16.0 / 9.0;
+    int image_width = 400;
+
+    // Calculate the image height, and ensure that it's at least 1.
+    int image_height = static_cast<int>(image_width / aspect_ratio);
+    image_height = (image_height < 1) ? 1 : image_height;
+
+    // Camera
+    auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * (static_cast<double>(image_width) / image_height);
+    auto camera_center = point3(0, 0, 0);
+
+    // Calculate the vectors across the horizontal and down the vertical viewport edges.
+    auto viewport_u = vec3(viewport_width, 0, 0);
+    auto viewport_v = vec3(0, -viewport_height, 0);
+
+    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+    auto pixel_delta_u = viewport_u / image_width;
+    auto pixel_delta_v = viewport_v / image_height;
+
+    // Calculate the location of the upper left pixel.
+    auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
     // Render
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
