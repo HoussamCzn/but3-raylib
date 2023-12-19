ef354341d9f575e75208bdec6dade1b606df058a
Houssam CUMZAIN <doriancoriace@gmail.com>
2023-12-19 10:31:12 +0100

    feat: add ray class

---
+++ include/ray.hxx
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "vec3.hxx"
+
+using point3 = vec3;
+
+class ray
+{
+public:
+
+    constexpr ray() = default;
+
+    constexpr ray(point3 const& origin, vec3 const& direction) : m_origin(origin), m_direction(direction) {}
+
+    constexpr auto origin() const -> point3 { return m_origin; }
+
+    constexpr auto direction() const -> vec3 { return m_direction; }
+
+    constexpr auto at(double t) const -> point3 { return m_origin + t * m_direction; }
+
+private:
+
+    point3 m_origin;
+    vec3 m_direction;
+};
