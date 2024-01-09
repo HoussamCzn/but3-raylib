4df4b553eea105e1638185125bcc6fe77339d5ba
Houssam CUMZAIN <doriancoriace@gmail.com>
2024-01-09 11:26:40 +0100

    test: add triangle rendering using metal material

--- source/main.cxx
+++ source/main.cxx
@@ -2,19 +2,15 @@
 #include "hittable_list.hxx" // hittable_list
 #include "material.hxx"      // lambertian, metal
 #include "sphere.hxx"        // sphere
+#include "triangle.hxx"
+#include "vec3.hxx"
 
 auto main() -> int
 {
-    auto const material_ground = std::make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto const material_center = std::make_shared<lambertian>(color(0.7, 0.3, 0.3));
-    auto const material_left = std::make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
-    auto const material_right = std::make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
+    auto const metal_material = std::make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
 
     hittable_list world;
-    world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
-    world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
-    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
-    world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
+    world.add(make_shared<triangle>(vec3(0, 0, -1), vec3(0.6, 0, -1), vec3(0, 0.4, -1), metal_material));
 
     camera view;
     view.aspect_ratio = 16.0 / 9.0;
