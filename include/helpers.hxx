0a43564789e67d9096f25c2140ac482597c0a627
Houssam CUMZAIN <doriancoriace@gmail.com>
2024-01-09 06:02:04 +0100

    feat: add random generator to helpers

--- include/helpers.hxx
+++ include/helpers.hxx
@@ -1,5 +1,16 @@
 #pragma once
 
-#include "constants.hxx" // infinity
+#include "constants.hxx" // pi
+
+#include <random> // std::mt19937, std::random_device, std::uniform_real_distribution
 
 constexpr auto degrees_to_radians(double degrees) -> double { return degrees * pi / 180.0; }
+
+inline auto random_double() -> double
+{
+    static std::random_device rd;
+    static std::mt19937 generator(rd());
+    static std::uniform_real_distribution<> distribution(0.0, 1.0);
+
+    return distribution(generator);
+}
