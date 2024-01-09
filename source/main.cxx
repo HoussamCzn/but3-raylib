2a46f63ad773d9d0d0f27f1bca021339a5216c03
Houssam CUMZAIN <doriancoriace@gmail.com>
2024-01-09 09:06:23 +0100

    feat: create few materials

--- source/main.cxx
+++ source/main.cxx
@@ -1,12 +1,20 @@
 #include "camera.hxx"        // camera
 #include "hittable_list.hxx" // hittable_list
+#include "material.hxx"      // lambertian, metal
 #include "sphere.hxx"        // sphere
 
 auto main() -> int
 {
+    auto const material_ground = std::make_shared<lambertian>(color(0.8, 0.8, 0.0));
+    auto const material_center = std::make_shared<lambertian>(color(0.7, 0.3, 0.3));
+    auto const material_left = std::make_shared<metal>(color(0.8, 0.8, 0.8));
+    auto const material_right = std::make_shared<metal>(color(0.8, 0.6, 0.2));
+
     hittable_list world;
-    world.add(std::make_shared<sphere>(point3(0, 0, -1), 0.5));
-    world.add(std::make_shared<sphere>(point3(0, -100.5, -1), 100));
+    world.add(make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
+    world.add(make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
+    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+    world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
     camera view;
     view.aspect_ratio = 16.0 / 9.0;
