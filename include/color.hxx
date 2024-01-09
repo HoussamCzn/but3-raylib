88be300ff13dfe2c5e383edca2da678e876bc697
Houssam CUMZAIN <doriancoriace@gmail.com>
2024-01-09 06:02:29 +0100

    feat: add samples_per_pixel setting to write_color

--- include/color.hxx
+++ include/color.hxx
@@ -1,19 +1,28 @@
 #pragma once
 
-#include "vec3.hxx" // vec3
+#include "interval.hxx" // interval
+#include "vec3.hxx"     // vec3
 
-#include <iostream> // std::ostream
+#include <algorithm> // std::ranges::transform
+#include <cstdint>   // uint32_t
+#include <iostream>  // std::ostream
 
 using color = vec3;
 
 /**
  * @brief Write the translated [0,255] value of each color component.
  *
- * @param out         The output stream.
- * @param pixel_color The color to write.
+ * @param out               The output stream.
+ * @param pixel_color       The color to write.
+ * @param samples_per_pixel The number of samples per pixel.
  */
-inline void write_color(std::ostream& out, color pixel_color)
+inline auto write_color(std::ostream& out, color pixel_color, uint32_t samples_per_pixel) -> void
 {
-    out << static_cast<int>(255.999 * pixel_color.x()) << ' ' << static_cast<int>(255.999 * pixel_color.y()) << ' '
-        << static_cast<int>(255.999 * pixel_color.z()) << '\n';
+    static constexpr interval intensity(0.000, 0.999);
+    auto const scale = 1.0 / samples_per_pixel;
+    std::ranges::transform(pixel_color.e, pixel_color.e.begin(), [scale](auto const component) { return component * scale; });
+    auto const [r, g, b] = pixel_color.e;
+
+    out << static_cast<uint32_t>(256 * intensity.clamp(r)) << ' ' << static_cast<uint32_t>(256 * intensity.clamp(g)) << ' '
+        << static_cast<uint32_t>(256 * intensity.clamp(b)) << '\n';
 }
