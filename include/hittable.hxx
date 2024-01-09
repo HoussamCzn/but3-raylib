75f2728e054f309ddef7346870adcc35f956068c
Houssam CUMZAIN <doriancoriace@gmail.com>
2024-01-09 02:57:32 +0100

    feat: create base class for hittable objects

---
+++ include/hittable.hxx
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "ray.hxx" // ray
+
+class hit_record
+{
+public:
+
+    auto set_face_normal(ray const& r, vec3 const& outward_normal) -> void
+    {
+        front_face = dot(r.direction(), outward_normal) < 0.0;
+        normal = front_face ? outward_normal : -outward_normal;
+    }
+
+    point3 p;
+    vec3 normal;
+    double t{0.0};
+    bool front_face{false};
+};
+
+class hittable
+{
+public:
+
+    virtual ~hittable() = default;
+
+    virtual auto hit(ray const& r, double t_min, double t_max, hit_record& rec) const -> bool = 0;
+};
