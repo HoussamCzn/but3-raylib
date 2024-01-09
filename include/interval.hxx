348245ca4d2179c6fde1dd8dbbb35a9d021c2a59
Houssam CUMZAIN <doriancoriace@gmail.com>
2024-01-09 06:01:38 +0100

    feat: add clamp function to interval

--- include/interval.hxx
+++ include/interval.hxx
@@ -2,6 +2,8 @@
 
 #include "constants.hxx" // infinity
 
+#include <algorithm> // std::clamp
+
 class interval
 {
 public:
@@ -10,9 +12,11 @@ public:
 
     constexpr interval(double min, double max) : min(min), max(max) {}
 
-    auto contains(double value) const -> bool { return min <= value && value <= max; }
+    constexpr auto contains(double value) const -> bool { return min <= value && value <= max; }
 
-    auto surrounds(double value) const -> bool { return min < value && value < max; }
+    constexpr auto surrounds(double value) const -> bool { return min < value && value < max; }
+
+    constexpr auto clamp(double value) const -> double { return std::clamp(value, min, max); }
 
     double min{infinity};
     double max{-infinity};
